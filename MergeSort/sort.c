deff63114f901c950c6948e24a38500628a80fa9
lixiong <2749811373@qq.com>
2018-10-03 16:58:14 +0800

    非递归归并排序

--- MergeSort/sort.c
+++ MergeSort/sort.c
@@ -41,6 +41,38 @@ void MergeSort(int arr[],int size)
   int *temp = (int *)malloc(sizeof(int)*size);
   assert(temp);
   _MergeSort(arr,0,size,temp);
+  free(temp);
+  temp = NULL;
+}
+
+
+void MergeSortNor(int arr[],int size)
+{
+  int *temp = (int *)malloc(sizeof(int)*size);
+  assert(temp);
+  int gap = 1;
+  while(gap<size)
+  {
+    int i = 0;
+    for(i = 0;i<size;i +=2*gap)
+    {
+      int left = i;
+      int mid = i+gap;
+      int right = mid+gap;
+
+      //判断mid和right是否越界
+      if(mid>size)
+        mid = size;
+      if(right>size)
+        right = size;
+
+      MergeData(arr,left,mid,right,temp);
+    }
+    gap <<=1;
+    memcpy(arr,temp,sizeof(int)*size);
+  }
+  free(temp);
+  temp = NULL;
 }
 
 
@@ -56,9 +88,10 @@ void PrintSort(int arr[],int size)
 
 void Sorttest()
 {
-  int arr[] = {2,4,6,7,1,8,9,0,3,5};
+  //int arr[] = {2,4,6,7,1,8,9,0,3,5};
+  int arr[] = {3,2,1,3,2,1};
   int size = sizeof(arr)/sizeof(arr[0]);
   PrintSort(arr,size);
-  MergeSort(arr,size);
+  MergeSortNor(arr,size);
   PrintSort(arr,size);
 }
